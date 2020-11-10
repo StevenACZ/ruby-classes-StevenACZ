f2ce37541855e729bbd94c2a2b1eaaf7cb78322d
stevenacz <scoailazaa@gmail.com>
2020-11-10 11:24:45 -0500

    Problema 17

--- 17dice/dice.rb
+++ 17dice/dice.rb
@@ -1,3 +1,13 @@
 class Dice
-  # Your code here
+  @@num_rolls = 0
+
+  def roll
+    @@num_rolls += Random.rand(7)
+  end
+  def reset
+    @@num_rolls = 0
+  end
+  def num_rolls
+    @@num_rolls
+  end
 end
